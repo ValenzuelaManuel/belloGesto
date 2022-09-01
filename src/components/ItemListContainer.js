f42a9b70a12de70feac592d46006d11ea9cf77bb
ValenzuelaManuel <m.valenzuela320@gmail.com>
2022-08-31 23:34:20 -0400

    Desafio Landing 31Ago

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,7 +1,9 @@
 import React from "react";
+
 function ItemListContainer () {
     return (
-<h2>Listado de productos disponibles</h2>
+        <h2>Listado de productos disponibles</h2>
     )
 }
+
 export default ItemListContainer
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,26 +5,26 @@ const NavBar= () => {
     return (
     <nav className="navbar navbar-expand-lg bg-light">
         <div className="container-fluid">
-        <a className="navbar-brand" href="#">BELLO GESTO - JOYAS</a>
+            <a className="navbar-brand" href="#">BELLO GESTO - JOYAS</a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-            <li className="nav-item">
-                <a className="nav-link" href="#">Productos</a>
-                </li>
-                <li className="nav-item">
-                <a className="nav-link" href="#">Nosotros</a>
-                </li>
-                <li className="nav-item">
-                <a className="nav-link" href="#">Contacto</a>
-                </li>
-            </ul>
-            <CartWidget/>
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Productos</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Nosotros</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Contacto</a>
+                    </li>
+                    </ul>
+                <CartWidget/>
             </div>
-        </div>
-</nav>
+        </div>  
+    </nav>
     )
 }
 export default NavBar;
